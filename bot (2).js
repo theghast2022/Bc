3c860d65721cd5887b95a98e929d9c7a36fa14e7
theghast2022 <44522450+theghast2022@users.noreply.github.com>
2018-11-03 00:54:07 +0100

    Update bot (2).js

--- bot (2).js
+++ bot (2).js
@@ -1,18 +1,47 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-console.log("Scrpit By Dream");
+var prefix = ">>";
 
+client.on("message", message => {
 
-client.on("ready", () => {
-let channel =     client.channels.get("505715759402582018")
-setInterval(function() {
-channel.send(`af`)
-channel.send(`as`)
-channel.send(`asfdgsfgsfg`)
-channel.send(`hdfh`)
-
-}, 25)
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
 })
- 
- 
-client.login('NTA1NzExNjU0OTc4MTkxMzgy.DrXo8Q.30R0dU4-XWRaQW06VkEtz2mHRVo');
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : i1Suhaib`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`M♡H Forever`,"http://twitch.tv/idk")
+client.user.setStatus("dnd")
+});
+
+client.on("message", message => {
+
+    if (message.content.startsWith(prefix + "bco")) {
+                 if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+let args = message.content.split(" ").slice(1);
+var argresult = args.join(' '); 
+message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+m.send(`${argresult}\n ${m}`);
+})
+message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+message.delete(); 
+};     
+});
+
+
+
+client.login("NTA4MDY2MjA3MzY3NDMwMTYz.Dr51Fg.HLmqO0xHKTwfCsdKzTM67b1friE");
