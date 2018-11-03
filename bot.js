b985c550a284afe4fd6281b66054e11b326986c2
theghast2022 <44522450+theghast2022@users.noreply.github.com>
2018-11-03 03:25:20 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -42,6 +42,34 @@ message.delete();
 };     
 });
 
+client.on('message', message => {
+var prefix = ">>";
+
+    if (message.author.id === client.user.id) return;
+    if (message.guild) {
+   let embed = new Discord.RichEmbed()
+    let args = message.content.split(' ').slice(1).join(' ');
+if(message.content.split(' ')[0] == prefix + 'bce') {
+    if (!args[1]) {
+message.channel.send("**.bc <message>**");
+return;
+}
+        message.guild.members.forEach(m => {
+   if(!message.member.hasPermission('ADMINISTRATOR')) return;
+            var bc = new Discord.RichEmbed()
+            .addField('» السيرفر :', `${message.guild.name}`)
+            .addField('» المرسل : ', `${message.author.username}#${message.author.discriminator}`)
+            .addField(' » الرسالة : ', args)
+            .setColor('#ff0000')
+            // m.send(`[${m}]`);
+            m.send(`${m}`,{embed: bc});
+        });
+    }
+    } else {
+        return;
+    }
+});
+
 
 
 client.login("NTA4MDY2MjA3MzY3NDMwMTYz.Dr52sQ.Qgt-a_rFUDK-0Nr0qrwo6CysnMg");
